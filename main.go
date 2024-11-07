eaf2105b1104b08ca2bfd793f3c77a3715a51377
cakedbake <mail@aivaras.xyz>
2024-11-07 22:37:50 +0200

    fifth commit

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 )
 
 func main() {
-	// Specify the filename
-	filename := "example.txt"
-
-	// Read the file into a string
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Print the contents of the string
-	fmt.Println(string(data))
+	fmt.Println("poo poo penis 💔")
 }
